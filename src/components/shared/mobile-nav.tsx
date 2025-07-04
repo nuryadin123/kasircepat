4b2839f885e356ec1bb33b7feda3cf9acddacbe9
Ali Nuryadin <nuryadinali1@gmail.com>
2025-07-04 00:28:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div rol

--- src/components/shared/mobile-nav.tsx
+++ src/components/shared/mobile-nav.tsx
@@ -49,10 +49,14 @@ export function MobileNav() {
           <Link
             href="/dashboard"
             onClick={handleLinkClick}
-            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            className="group flex items-center gap-2 text-lg font-semibold"
           >
-            <Terminal className="h-4 w-4 transition-all group-hover:scale-110" />
-            <span className="sr-only">{storeName}</span>
+            <div
+              className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            >
+              <Terminal className="h-4 w-4 transition-all group-hover:scale-110" />
+            </div>
+            <span>{storeName}</span>
           </Link>
           {navItems.map((item) => (
             <Link
